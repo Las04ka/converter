c0938845fdaa1a2a60ec2e87d912bda81d609dd5
Las04ka <denkostyuk@gmail.com>
2023-05-31 19:11:42 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CurrencyConverterComponent } from './currency-converter/currency-converter.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, CurrencyConverterComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
